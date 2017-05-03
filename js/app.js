fff7d95bf91acb3251b02d1dc3f10c4dc2274167
ksurtel <kamil.surtel@gmail.com>
2017-05-03 17:20:05 +0200

    Added winning conditions

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   var guessCount = 0;
   var guess1 = "";
   var guess2 = "";
+  var matched = [];
 
   function createGrid(n) {
     getImages(n);
@@ -19,15 +20,15 @@ document.addEventListener("DOMContentLoaded", function (event) {
   function getImages(n) {
     for (var i = 1; i <= n / 2; i++) {
       var rand = Math.floor(Math.random() * (500 - 0)) + 0;
-      image.push("https://unsplash.it/100/150?image=" + rand);
-      image.push("https://unsplash.it/100/150?image=" + rand);
+      image.push(`https://unsplash.it/100/150?image=+${rand}`);
+      image.push(`https://unsplash.it/100/150?image=+${rand}`);
     }
   }
 
   function createTiles(n) {
     for (var i = 0; i < n; i++) {
       var newTile = document.createElement("div");
-      newTile.innerHTML = "<img src=" + image[i] + " class='faceDown'><img src='back.jpg' class='faceUp'>";
+      newTile.innerHTML = `<img src='${image[i]}' class='faceDown'><img src='back.jpg'>`;
       newTile.className = "tile";
       tile.push(newTile);
     }
@@ -50,19 +51,24 @@ document.addEventListener("DOMContentLoaded", function (event) {
   }
 
   function match() {
-    console.log(guessCount);
     if (guessCount === 0) {
-      this.classList.toggle("flipper")
+      this.classList.toggle("flipper");
       guess1 = this;
       guessCount += 1;
     } else if (guessCount === 1 && guess1 !== this) {
-      this.classList.toggle('flipper')
+      this.classList.toggle('flipper');
       guess2 = this;
       guessCount += 1;
       if (guess1.firstChild.getAttribute("src") === guess2.firstChild.getAttribute("src")) {
         guess1.removeEventListener('click', match);
         guess2.removeEventListener('click', match);
         guessCount = 0;
+        guess1.classList.add('matched');
+        guess2.classList.add('matched');
+        matched.push(guess1, guess2);
+        if (matched.length === tile.length) {
+          console.log('winner winner chicken dinner');
+        }
       } else {
         window.setTimeout(function () {
           guess1.classList.toggle('flipper');
@@ -71,7 +77,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
         }, 2000);
       }
     }
-    console.log(guessCount);
   }
 
   function addEvent() {
@@ -79,6 +84,5 @@ document.addEventListener("DOMContentLoaded", function (event) {
       tile[i].addEventListener('click', match);
     }
   }
-
-  createGrid(36);
+  createGrid(6);
 });
